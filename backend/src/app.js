afd38bc267d68cbfb5ac97945cc2048765398e34
shyam poudel <poudelshyam844@gmail.com>
2025-08-22 11:04:21 +0530

    Refactor CORS configuration for simplicity and clarity

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,7 @@ import {upload} from "./multer.middleware.js";
 import dotenv from "dotenv"
 dotenv.config()
 const app =express(); 
-app.use(cors({
-    origin: [
-        process.env.CORS_ORIGIN
-    ],
-    credentials: true
-}))
+app.use(cors({ origin: true, credentials: true }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
